86466dc157b39b2fda6685392d558d2882e66f3d
Stupel <hellomariandev@gmail.com>
2024-03-04 23:43:39 +0100

    Update product-gallery.tsx

--- components/product-gallery.tsx
+++ components/product-gallery.tsx
@@ -1,8 +1,22 @@
-// component to display products images
+"use client";
 
-export default function ProductGallery() {
-    return (
-      <div>ProductGallery</div>
-    )
-  }
-  
+import { Product } from "@/lib/types";
+
+interface Props {
+  product: Product;
+}
+
+export default function ProductGallery({ product }: Props) {
+  return (
+    <div className="bg-slate-100 py-6">
+      <div className="aspect-h-1 aspect-w-1 w-full grid place-items-center mb-4">
+        <div
+          className="flex-none w-96 h-96 rounded-lg py-4 px-2 inline-block hover:scale-105 ease-in-out duration-300 scroll-smooth cursor-pointer"
+          style={{
+            background: "linear-gradient(to bottom, #ccc, #999)",
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+}
