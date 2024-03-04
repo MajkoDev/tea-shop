56df269b57a7232989a23d884f3ab21cc59163b7
Stupel <hellomariandev@gmail.com>
2024-03-04 23:43:43 +0100

    Update product-info.tsx

--- components/product-info.tsx
+++ components/product-info.tsx
@@ -1,8 +1,19 @@
-// component to display detailed product information
+"use client";
 
-export default function ProductInfo() {
-    return (
-      <div>ProductInfo</div>
-    )
-  }
-  
+import { Product } from "@/lib/types";
+
+interface Props {
+  product: Product;
+}
+
+export default function ProductInfo({ product }: any) {
+  return (
+    <div className="bg-slate-50 py-6">
+      <h1 className="font-extralight text-3xl text-center">
+        Product Informations
+      </h1>
+    </div>
+  );
+}
+
+// component to display detailed product information
