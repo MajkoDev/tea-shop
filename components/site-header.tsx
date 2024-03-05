9be83886941204d57322c5bac21d40e42373c465
Stupel <hellomariandev@gmail.com>
2024-03-05 09:28:36 +0100

    site header

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,17 +1,22 @@
 import Link from "next/link";
-
-import {
-  NavigationMenu,
-  NavigationMenuList,
-  NavigationMenuItem,
-  NavigationMenuTrigger,
-  NavigationMenuContent,
-} from "./ui/navigation-menu";
+import { Button } from "./ui/button";
+import { ModeToggle } from "./theme-toggle";
 
 export default function SiteHeader() {
   return (
     <header className="supports-backdrop-blur:bg-background/90 sticky top-0 z-50 w-full border-b bg-background/90 backdrop-blur mb-4 px-[1.4rem] md:px-[4rem] lg:px-[6rem] xl:px-[8rem] 2xl:px-[12rem]">
-      <div className="flex h-14 items-center"></div>
+      <div className="flex h-14 justify-between items-center">
+        <NavLogo />
+        <div className="flex gap-1 items-center">
+          <Link href="/cart">
+            <Button variant="outline" className="h-8 rounded-lg">
+              Shopping Cart
+            </Button>
+          </Link>
+          <Button className="h-8 rounded-lg">Log In</Button>
+          <ModeToggle />
+        </div>
+      </div>
     </header>
   );
 }
@@ -21,15 +26,5 @@ function NavLogo() {
     <div className="">
       <span className="text-lg font-semibold">Silk Road Tea House</span>
     </div>
-  );
-}
-
-function NavMenu() {
-  return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem></NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
   );
 }
