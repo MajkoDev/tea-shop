188968adec090f640dde2666f244c22e9028ba6c
Stupel <hellomariandev@gmail.com>
2024-03-05 09:28:41 +0100

    site navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,21 +1,125 @@
+"use client";
+
 import Link from "next/link";
-import { ModeToggle } from "./theme-toggle";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "./ui/navigation-menu";
+import { cn } from "@/lib/utils";
+
+const teas = [
+  { label: "Green Tea", value: "green-tea" },
+  { label: "White Tea", value: "white-tea" },
+  { label: "Black Tea", value: "black-tea" },
+  { label: "Oolong Tea", value: "oolong-tea" },
+  { label: "Red Tea (Pu Ehr)", value: "pu-ehr" },
+];
 
 export default function Navbar() {
   return (
-    <header className="w-full">
-      <div className="my-4 px-12 lg:px-24 flex flex-row justify-between items-center">
-        <h1 className="text-xl font-semibold">Logo</h1>
-        <div className="flex flex-row gap-2 items-center">
-          <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
-          <Link href="/products">Products</Link>
-          <Link href="/blog">Blog</Link>
-          <Link href="/cart">Cart</Link>
+    <nav className="supports-backdrop-blur:bg-background/90 sticky top-0 z-50 w-full border-b bg-background/90 backdrop-blur mb-4 px-[1.4rem] md:px-[4rem] lg:px-[6rem] xl:px-[8rem] 2xl:px-[12rem]">
+      <div className="my-4 px-12 lg:px-24">
+        <NavigationMenu className="max-w-full">
+          <NavigationMenuList className="flex flex-row items-center justify-between gap-6 lg:gap-16 xl:gap-24 max-w-full">
+            <NavigationMenuItem>
+              <Link href="/" legacyBehavior passHref>
+                <NavigationMenuLink>Home</NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
 
-          <ModeToggle />
-        </div>
+            <NavigationMenuItem>
+              <Link href="/about" legacyBehavior passHref>
+                <NavigationMenuLink>About</NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+
+            <NavigationMenuItem>
+              <NavigationMenuTrigger className="text-md font-[400]">
+                Teas
+              </NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                  {teas.map((tea) => (
+                    <li key={tea.value}>
+                      <NavigationMenuLink asChild>
+                        <a
+                          className={cn(
+                            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                          )}
+                        >
+                          <div className="text-sm font-medium leading-none">
+                            {tea.label}
+                          </div>
+                        </a>
+                      </NavigationMenuLink>
+                    </li>
+                  ))}
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+
+            <NavigationMenuItem>
+              <NavigationMenuTrigger className="text-md font-[400]">
+                Tea Accessories
+              </NavigationMenuTrigger>
+              <NavigationMenuContent></NavigationMenuContent>
+            </NavigationMenuItem>
+
+            <NavigationMenuItem>
+              <Link href="/blog" legacyBehavior passHref>
+                <NavigationMenuLink>Blog</NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
       </div>
-    </header>
+    </nav>
+  );
+}
+
+export function NavigationMenuDemo() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger></NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              <li className="row-span-3">
+                <NavigationMenuLink asChild>
+                  <a
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/"
+                  >
+                    <div className="mb-2 mt-4 text-lg font-medium">
+                      shadcn/ui
+                    </div>
+                    <p className="text-sm leading-tight text-muted-foreground">
+                      Beautifully designed components built with Radix UI and
+                      Tailwind CSS.
+                    </p>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+              <Link href="/docs" title="Introduction">
+                Re-usable components built using Radix UI and Tailwind CSS.
+              </Link>
+              <Link href="/docs/installation" title="Installation">
+                How to install dependencies and structure your app.
+              </Link>
+              <Link href="/docs/primitives/typography" title="Typography">
+                Styles for headings, paragraphs, lists...etc
+              </Link>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
   );
 }
