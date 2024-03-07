ff1b891fa6ab6e45a9006b8fed0c91f0e39d68d5
Stupel <hellomariandev@gmail.com>
2024-03-07 20:19:13 +0100

    footer n header

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,69 +1,73 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
-
+import React from "react";
 import Link from "next/link";
+
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuTrigger,
+  NavigationMenuContent,
+  NavigationMenuLink,
+} from "./ui/navigation-menu";
 import { Button } from "./ui/button";
 import { ModeToggle } from "./theme-toggle";
-import Navbar from "./navbar";
 
 export default function SiteHeader() {
-  const [isScrolled, setIsScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
-    <header
-      className={`${
-        isScrolled
-          ? "supports-backdrop-blur:bg-background/80"
-          : "supports-backdrop-blur:bg-background/90 "
-      } sticky top-0 z-50 w-full border-b bg-background/90 backdrop-blur px-[1.4rem] md:px-[4rem] lg:px-[6rem] xl:px-[8rem] 2xl:px-[12rem]`}
-    >
-      <div
-        className={`flex justify-between items-center transition-height duration-300 ${
-          isScrolled ? "pt-1" : "my-3"
-        } `}
-      >
-        <NavLogo />
-        <div className="flex gap-1 items-center">
-          <Link href="/cart">
-            <Button variant="outline" className="h-8 rounded-lg">
-              Shopping Cart
-            </Button>
-          </Link>
-          <Button className="h-8 rounded-lg">Log In</Button>
-          <ModeToggle />
+    <header className="w-full h-[7vh] top-0 z-50 border-b bg-background/90 backdrop-blur px-[1.4rem] md:px-[4rem] lg:px-[6rem] xl:px-[8rem] 2xl:px-[12rem]">
+      <div className="h-full flex flex-row items-center justify-between">
+        <Link href="/">
+          <h1 className="text-lg font-bold">Tea House</h1>
+        </Link>
+
+        <div className="flex flex-row gap-1">
+          <NavigationMenu className="hidden md:block">
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <Button variant="ghost" className="rounded-lg">
+                  Tea
+                </Button>
+              </NavigationMenuItem>
+
+              <NavigationMenuItem>
+                <NavigationMenuTrigger className="rounded-lg">
+                  Other Products
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <Link href="/accessories">
+                    <ListItem>Tea Accessories</ListItem>
+                  </Link>
+
+                  <Link href="/courses">
+                    <ListItem>Tea Courses</ListItem>
+                  </Link>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+          <div className="flex gap-1 items-center">
+            <Link href="/cart">
+              <Button variant="outline" className="rounded-lg">
+                Shopping Cart
+              </Button>
+            </Link>
+            <Button className="rounded-lg">Log In</Button>
+            <ModeToggle />
+          </div>
         </div>
-      </div>
-      <div
-        className={`px-12 lg:px-24 transition-height duration-300 ${
-          isScrolled ? "my-1" : "my-3"
-        }`}
-      >
-        <Navbar />
       </div>
     </header>
   );
 }
 
-function NavLogo() {
+const ListItem = ({ children }: { children: string }) => {
   return (
-    <div className="">
-      <span className="text-lg font-semibold">Silk Road Tea House</span>
-    </div>
+    <NavigationMenuLink asChild>
+      <a className="w-44 block select-none space-y-1 rounded-md p-3 no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
+        <h3 className="text-sm font-medium">{children}</h3>
+      </a>
+    </NavigationMenuLink>
   );
-}
+};
