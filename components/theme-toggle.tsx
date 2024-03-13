e59f78d985f5a68bdf54554c5986a6c56427b81a
Stupel <hellomariandev@gmail.com>
2024-03-13 21:41:59 +0100

    Update theme-toggle.tsx

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -16,22 +16,25 @@ export function ModeToggle() {
   const { setTheme } = useTheme();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon" className="h-8 rounded-lg">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div className='m-2'>
+      
+      <Button
+        variant="ghost"
+        size="icon"
+        className="h-8 w-8 rounded-lg dark:hidden"
+        onClick={() => setTheme("dark")}
+      >
+        <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      </Button>
+      <Button
+        variant="ghost"
+        size="icon"
+        className="h-8 w-8 rounded-lg hidden dark:inline-block dark:visible"
+        onClick={() => setTheme("light")}
+      >
+        <span>Dark</span>
+        <MoonIcon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all" />
+      </Button>
+    </div>
   );
 }
